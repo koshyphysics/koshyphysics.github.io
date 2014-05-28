5ccef9f8fcf3ca017c2caed4c5b7f577ec14bf7c
Phil <birkal@outlook.com>
2014-05-27 23:00:54 -0700

    Reorganization

---
+++ js/test-simulation-3.js
@@ -0,0 +1,48 @@
+var Engine = Matter.Engine,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    Body = Matter.Body,
+    Composite = Matter.Composite,
+    Composites = Matter.Composites,
+    Common = Matter.Common,
+    Constraint = Matter.Constraint,
+    RenderPixi = Matter.RenderPixi,
+    Events = Matter.Events,
+    Bounds = Matter.Bounds,
+    Vector = Matter.Vector,
+    Vertices = Matter.Vertices,
+    MouseConstraint = Matter.MouseConstraint,
+    Mouse = Matter.Mouse,
+    Query = Matter.Query;
+
+var engine = Engine.create(document.body, {
+    render: {
+        options: {
+            showAngleIndicator: true,
+            wireframes: false,
+        }
+    }
+});
+
+var mouseConstraint = MouseConstraint.create(engine);
+World.add(engine.world, mouseConstraint);
+
+World.add(engine.world, Bodies.circle(engine.render.options.width * 0.3, engine.render.options.height * 0.8, 10));
+
+var offset = 5;
+World.add(engine.world, [
+    Bodies.rectangle(engine.render.options.width / 2, -offset, engine.render.options.width + 2 * offset, 50, {
+        isStatic: true
+    }),
+    Bodies.rectangle(engine.render.options.width / 2, engine.render.options.height + offset, engine.render.options.width + 2 * offset, 50, {
+        isStatic: true
+    }),
+    Bodies.rectangle(engine.render.options.width + offset, engine.render.options.height / 2, 50, engine.render.options.height + 2 * offset, {
+        isStatic: true
+    }),
+    Bodies.rectangle(-offset, engine.render.options.height / 2, 50, engine.render.options.height + 2 * offset, {
+        isStatic: true
+    })
+]);
+
+Engine.run(engine);
